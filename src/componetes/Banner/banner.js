28d93bedc16477a61ac82f8e13fd1d4a950419e1
Rodrigo domingues da silva <rodrigo.silva@wetalkie.com>
2024-08-10 09:40:48 -0300

    inicio

---
+++ src/componetes/Banner/banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className='banner'>
+            <img alt="Banner principal" src="/imagens/banner.png"></img>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/componetes/CampoTexto/CampoTexto.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css'
+const CampoTexto = () => {
+    return (
+        <div className='campoTexto'>
+            <h1>Campo Texto</h1>
+        </div>
+    )
+}
+
+export default CampoTexto
